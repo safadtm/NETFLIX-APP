e47db672b347b5c8aec514d9a6f89c9916ce3dc3
safadtm <safadtm2004@gmail.com>
2022-06-17 16:15:57 +0530

    minimal frontview

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
 import './App.css'
+import React from 'react'
+import NavBar from './Components/Components/NavBar/NavBar'
+import Banner from './Components/Components/Banner/Banner'
+import RowPost from './Components/Components/RowPost/RowPost'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar/>
+      <Banner/>
+      <RowPost/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Banner.css'
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className="content">
+            <h1 className='title'>Movie Name</h1>
+            <div className="banner_buttons">
+                <button className='button'>Play</button>
+                <button className='button'>My list</button>
+            </div>
+            <h1 className="description">in publishing and graphic design ,lorem ipsum is a placeholder
+            text commonly used to demonstrate </h1>
+        </div>
+        <div className="fade_bottom"></div>
+
+    </div>
+  )
+}
+
+export default Banner
